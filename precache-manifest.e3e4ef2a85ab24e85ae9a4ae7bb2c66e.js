16ca49fd00c295beea8f6aee7f4b8280550e42ca
wizkid-07 <50694990+wizkid-07@users.noreply.github.com>
2020-04-10 12:07:47 +0530

    Updates

---
+++ precache-manifest.e3e4ef2a85ab24e85ae9a4ae7bb2c66e.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53f6fc3bcd2dc98f93d138dfc0d6b947",
+    "url": "/inkRobe/index.html"
+  },
+  {
+    "revision": "575e2f5f8dec9a67f0f2",
+    "url": "/inkRobe/static/css/main.594d6a57.chunk.css"
+  },
+  {
+    "revision": "c88ec70d3d3e88fef39d",
+    "url": "/inkRobe/static/js/2.5ec5758d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/inkRobe/static/js/2.5ec5758d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "575e2f5f8dec9a67f0f2",
+    "url": "/inkRobe/static/js/main.b0b0866e.chunk.js"
+  },
+  {
+    "revision": "849e518a65d87a9364a9",
+    "url": "/inkRobe/static/js/runtime-main.1aa2f401.js"
+  },
+  {
+    "revision": "0e7b09c3523e4f13f347b1fc8b31ff47",
+    "url": "/inkRobe/static/media/CTA.0e7b09c3.png"
+  },
+  {
+    "revision": "77489acb542c0c77e4b64fe4654e940b",
+    "url": "/inkRobe/static/media/bg.77489acb.mp4"
+  },
+  {
+    "revision": "a2af72b7ab5ef98b5dd86f9167cf5736",
+    "url": "/inkRobe/static/media/circle.a2af72b7.svg"
+  },
+  {
+    "revision": "dbfd861c1252ec4293f93967fa433310",
+    "url": "/inkRobe/static/media/client6.dbfd861c.png"
+  },
+  {
+    "revision": "90c625bb4499b8083eb52b8f4f7f92af",
+    "url": "/inkRobe/static/media/demo_shirt.90c625bb.jpg"
+  },
+  {
+    "revision": "ebf6d7cafe0e191a1c58269816b0868b",
+    "url": "/inkRobe/static/media/image.ebf6d7ca.png"
+  },
+  {
+    "revision": "947bcf6184cf95be2080ee493bb691d3",
+    "url": "/inkRobe/static/media/menu_icon.947bcf61.svg"
+  },
+  {
+    "revision": "643fe22ab11bf1941c4f83f42b35a588",
+    "url": "/inkRobe/static/media/menu_red.643fe22a.svg"
+  },
+  {
+    "revision": "6f775b42f0fb78b2b555232904e13283",
+    "url": "/inkRobe/static/media/next.6f775b42.png"
+  },
+  {
+    "revision": "25ccea3d589ee215a12831ea5ed62f89",
+    "url": "/inkRobe/static/media/official_bg.25ccea3d.mp4"
+  },
+  {
+    "revision": "9d461f289f47960cfd47726cffead88e",
+    "url": "/inkRobe/static/media/profile-pic.9d461f28.jpg"
+  },
+  {
+    "revision": "bee9054bbf0cb3b0853dab0129e6c82e",
+    "url": "/inkRobe/static/media/retail.bee9054b.svg"
+  },
+  {
+    "revision": "f87a31b098572dda19089af8a21248b6",
+    "url": "/inkRobe/static/media/slider.f87a31b0.png"
+  },
+  {
+    "revision": "f45b220684ba4164e5bce54f492d6faf",
+    "url": "/inkRobe/static/media/team.f45b2206.png"
+  },
+  {
+    "revision": "98485ee03bc16abf4156b1c7ce0d74e0",
+    "url": "/inkRobe/static/media/tshirt.98485ee0.svg"
+  },
+  {
+    "revision": "501a2b71b527a81ed41a30518b87891b",
+    "url": "/inkRobe/static/media/wholesale.501a2b71.svg"
+  }
+]);
